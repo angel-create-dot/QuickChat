0bfaf06371b49a54d49f134f75962476e7d0929e
angel-create-dot <st10533417@rcconnect.edu.za>
2026-09-17 16:17:29 +0200

    Add password complexity validation

--- src/main/java/za/com/softchat/quickchat/Login.java
+++ src/main/java/za/com/softchat/quickchat/Login.java
@@ -9,7 +9,28 @@ package za.com.softchat.quickchat;
  * @author Angel
  */
 public class Login {
-     public boolean checkUserName(String username) {
-       return username.contains("_") && username.length() <= 5; 
-    }   
+
+    public boolean checkUserName(String username) {
+        return username.contains("_") && username.length() <= 5;
+    }
+
+    public boolean checkPasswordComplexity(String password) {
+        boolean hasCapital = false;
+        boolean hasNumber = false;
+        boolean hasSpecial = false;
+
+        for (int i = 0; i < password.length(); i++) {
+            char currentCharacter = password.charAt(i);
+
+            if (Character.isUpperCase(currentCharacter)) {
+                hasCapital = true;
+            } else if (Character.isDigit(currentCharacter)) {
+                hasNumber = true;
+            } else if (!Character.isLetterOrDigit(currentCharacter)) {
+                hasSpecial = true;
+            }
+        }
+
+        return password.length() >= 8 && hasCapital && hasNumber && hasSpecial;
+    }
 }
