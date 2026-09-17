1aa3026d8ea30c77d6028c33348835987d89e93d
angel-create-dot <st10533417@rcconnect.edu.za>
2026-09-17 16:28:47 +0200

    Add user registration with validation

--- src/main/java/za/com/softchat/quickchat/Login.java
+++ src/main/java/za/com/softchat/quickchat/Login.java
@@ -9,6 +9,10 @@ package za.com.softchat.quickchat;
  * @author Angel
  */
 public class Login {
+private String registeredUsername;
+private String registeredPassword;
+private String firstName;
+private String lastName;
 
     public boolean checkUserName(String username) {
         return username.contains("_") && username.length() <= 5;
@@ -45,4 +49,26 @@ public class Login {
     public boolean checkCellPhoneNumber(String cellNumber) {
         return cellNumber.matches("^\\+27[0-9]{9}$");
     }
+    public String registerUser(String firstName, String lastName,
+        String username, String password, String cellNumber) {
+
+    if (!checkUserName(username)) {
+        return "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.";
+    }
+
+    if (!checkPasswordComplexity(password)) {
+        return "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.";
+    }
+
+    if (!checkCellPhoneNumber(cellNumber)) {
+        return "Cell phone number incorrectly formatted or does not contain international code.";
+    }
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.registeredUsername = username;
+    this.registeredPassword = password;
+
+    return "User registered successfully.";
+}
 }
